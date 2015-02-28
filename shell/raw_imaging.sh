20825ab303a354376e91c290d5b33f3d5592b07e
Shawn Nock <nock@nocko.se>
2015-02-28 09:15:12 -0600

    Adds support functions for btrfs port

--- shell/raw_imaging.sh
+++ shell/raw_imaging.sh
@@ -2,69 +2,146 @@ function save_mbr {
   dd if=$INTERNAL_DISK of=$RAW_IMAGE_DIR/windows.mbr bs=512 count=1
 }
 
-function save_images {
+function mount_windows {
+    mount $WINDOWS_PART $WINDOWS_DIR
+}
+
+function umount_windows {
+    umount $WINDOWS_DIR
+}
+
+function remove_windows_cruft {
+    mount_windows
+    for file in $WINDOWS_SHIT; do
+	if [ -e $WINDOWS_DIR/$file ]; then
+	    rm $WINDOWS_DIR/$file
+	fi
+    done
+    umount_windows
+}
+
+function save_windows {
   # This function makes new windows clone images
   save_mbr
-  ntfsclone ${INTERNAL_DISK}1 -O $RAW_IMAGE_DIR/windows.reserved.ntfs.img
-  mount ${INTERNAL_DISK}2 /mnt
-  /bin/rm /mnt/pagefile.sys
-  /bin/rm /mnt/hiberfil.sys
-  umount /mnt
-  ntfsclone ${INTERNAL_DISK}2 -O $RAW_IMAGE_DIR/windows.main.ntfs.img
+  remove_windows_cruft
+  image_file=$RAW_IMAGE_DIR/windows.ntfs.img
+  if [ ! -e $image_file ]; then
+      echo "save_windows: No previous image, creating new one"
+      ntfsclone ${WINDOWS_PART} -o $image_file
+  else
+      echo "save_windows: Updating existing windows image"
+      # So, if we unlink the file, btrfs cannot track the
+      # changes... so we jump through a hoop to only change the needed blocks
+      compare_image_update_in_place ${WINDOWS_PART} ${image_file}
+  fi
 }
 
-function clone_linux {
-  # Takes a snapshot of admin linux and mounts it on /mnt
-  mount_admin_snap
-  # Change a few things that makes the local linux different
-  /bin/cp -f /qcimage/linux_root/etc/local-fstab /mnt/etc/fstab
-  /bin/cp -f /qcimage/linux_root/boot/grub2/local-grub.cfg /mnt/boot/grub2/grub.cfg
-  sed -i -e 's/adminflash/local-linux/' /mnt/etc/sysconfig/network
-  # Don't copy the ntfs images to the local linux partition
-  rm -rf /mnt/images/*
-  rm -rf /lib/systemd/system/multi-user.target.wants/plymouth*
-  umount /mnt
-  # Clone the filesystem to it's new home
-  partclone.extfs -b -s/dev/vg_adminflash/player-root -O${INTERNAL_DISK}3
-  # Cleanup
-  umount_admin_snap
-  mount /dev/sda3 /mnt
-  # Reinstall grub
-  grub2-install --root-directory=/mnt --no-floppy $INTERNAL_DISK
-  cp /qcimage/linux_root/boot/grub2/altcmp.mod /mnt/boot/grub2
-  umount /mnt
+function clear_milestones {
+    fs=$1
+    btrfs subvolume list $fs | awk '/milestone/ {print $9}' |xargs -i btrfs subvolume delete $fs{}
 }
 
-function mount_admin_snap {
-  # Create an LVM snapshot called player root of the adminflash root
-  # Use all the remaining free extents in the volume group
-  lvcreate -s -nplayer-root -l+20 /dev/vg_adminflash/lv_root
-  mount /dev/vg_adminflash/player-root /mnt
+function gen_milestone {
+    snap_name=milestone_$(date +%s)
+    echo $snap_name > /.milestone
+    btrfs subvolume snapshot -r / ${SNAPSHOT_DIR}/$snap_name
 }
 
-function umount_admin_snap {
-  # Umount and delete the snapshot
-  umount /mnt
-  lvremove -f /dev/vg_adminflash/player-root
+function mount_client_linux {
+    if [ ! "$QCIMAGE_MODE" == "admin" ]; then
+	echo "Operation not valid in context $QCIMAGE_MODE: mount_client_linux"
+	return 1
+    fi
+    if [ ! "$(get_partition_fstype ${LOCAL_LINUX_PART})" == "btrfs" ]; then
+	echo "Found *unformated* local linux partition on $LOCAL_LINUX_PART, formatting" 
+	mkfs.btrfs ${LOCAL_LINUX_PART}	
+    fi
+    mount ${LOCAL_LINUX_PART} ${LOCAL_LINUX_DIR}
+    if [ ! -e ${LOCAL_LINUX_DIR}/${SNAPSHOT_DIR} ]; then
+	btrfs subvolume create ${LOCAL_LINUX_DIR}/${SNAPSHOT_DIR}
+    fi
 }
 
-function init_player_key {
-  # Expects device name e.g. /dev/sdq Grub will be installed on the
-  # device, and a partition created for FAT32 but the filesystem will
-  # not be created, we may be able to use "booted linux but player key
-  # has no filesystem" to automatically create the player diff the
-  # first time
-  label=`sanitize $HANDLE`
-  dosfslabel ${PLAYER_DISK} $label
-  cp -r /windows/.qcimage /joueur
-  init_player_settings
+function umount_client_linux {
+    if [ ! "$QCIMAGE_MODE" == "admin" ]; then
+	echo "Operation not valid in context $QCIMAGE_MODE: umount_client_linux"
+	return 1
+    fi
+    umount ${LOCAL_LINUX_DIR}
 }
 
-function sanitize {
-    # Truncate handle to FAT32 Compatible 11 char for label Lot's of
-    # weird charaters seem to work, IDK if we should strip other
-    # things
-    echo $1 | cut -n -b -11 -
+function set_btrfs_default_subvol_by_name {
+    fs_mountpoint=$1
+    subvol_name=$2
+    subvol_id=$(btrfs subvolume list ${fs_mountpoint}|awk -v subvol_name="$subvol_name" '{if ($9 == subvol_name) { print $2 }}')
+    if [ ! "$subvol_id" == "" ]; then
+	btrfs subvolume set-default ${subvol_id} ${fs_mountpoint}
+    else
+	echo "set_btrfs_default_subvol_by_name: Couldn't find subvol name = \"$subvol_name\" on mountpoint = \"$fs_mountpoint\""
+    fi
+}
+
+function mount_client_btrfs_root {
+    mount_client_linux
+    btrfs subvolume set-default 0 ${LOCAL_LINUX_DIR}
+    umount_client_linux
+    mount_client_linux
+}
+
+function mount_client_def_subvol {
+    umount_client_linux
+    mount_client_linux
+}
+
+function client_linux_is_mounted_p {
+    mountpoint=$(mount | awk -v part="$LOCAL_LINUX_PART" 'index($1, part) {print $3}')
+    if [ "$mountpoint" == "${LOCAL_LINUX_DIR}" ]; then
+	return 0
+    else
+	return 1
+    fi
+}
+
+function transfer_linux_to_client {
+    if [ ! "$QCIMAGE_MODE" == "admin" ]; then
+	echo "Operation not valid in context $QCIMAGE_MODE: transfer_linux_to_client"
+    fi
+    if client_linux_is_mounted_p; then
+	umount_client_linux
+    fi
+    mount_client_btrfs_root
+    # Delete old root subvol
+    if [ -e ${LOCAL_LINUX_DIR}/root ]; then
+	btrfs subvolume delete ${LOCAL_LINUX_DIR}/root
+    fi
+    snap_name=client_root_$(date +%s)
+    snap=$SNAPSHOT_DIR/$snap_name
+    ## Create a snapshot of admin linux root on admin linux
+    btrfs subvolume snapshot -r / $snap
+    sync
+    ## Send the snapshot to the local linux as /${snap}
+    btrfs send $snap | btrfs receive ${LOCAL_LINUX_DIR}/${SNAPSHOT_DIR}
+    ## Set this new local linux subvolume to be automatically mounted
+    btrfs subvolume snapshot ${LOCAL_LINUX_DIR}/${snap} ${LOCAL_LINUX_DIR}/root
+    set_btrfs_default_subvol_by_name ${LOCAL_LINUX_DIR} root
+    ## Remount client_linux to reflect new default subvolume / root
+    umount_client_linux
+    mount_client_linux
+    genfstab -U ${LOCAL_LINUX_DIR} > ${LOCAL_LINUX_DIR}/etc/fstab
+    modify_local_linux
+    configure_local_grub
+}
+
+function modify_local_linux {
+    echo "FIXME: Do things to differentiate files on local linux from USB boot"
+    #/bin/cp -f /qcimage/linux_root/etc/local-fstab /mnt/etc/fstab
+    #/bin/cp -f /qcimage/linux_root/boot/grub2/local-grub.cfg /mnt/boot/grub2/grub.cfg
+    #cp /qcimage/linux_root/boot/grub2/altcmp.mod /mnt/boot/grub2
+}
+
+function configure_local_grub {
+    grub-install --target=i386-pc --debug --no-floppy --root-dir=${LOCAL_LINUX_DIR} ${INTERNAL_DISK}
+    arch-chroot ${LOCAL_LINUX_DIR} /usr/bin/grub-mkconfig -o /boot/grub/grub.cfg.test222
 }
 
 function clone_new_machine {
@@ -72,42 +149,141 @@ function clone_new_machine {
   dd if=${RAW_IMAGE_DIR}/windows.mbr of=${INTERNAL_DISK}
   # Rescan the MBR and create the new partition devices
   partprobe
-  echo Cloning Windows reserved partition to ${INTERNAL_DISK}1
-  ntfsclone $RAW_IMAGE_DIR/windows.reserved.ntfs.img -O ${INTERNAL_DISK}1
-  echo Cloning Windows main partition to ${INTERNAL_DISK}2
-  ntfsclone $RAW_IMAGE_DIR/windows.main.ntfs.img -O ${INTERNAL_DISK}2
-  echo Cloning Linux to ${INTERNAL_DISK}3
-  clone_linux
+  reload
+  echo Cloning Windows main partition to ${WINDOWS_PART}
+  ntfsclone $RAW_IMAGE_DIR/windows.main.ntfs.img -O ${WINDOWS_PART}
+  echo Transferring Linux to ${LOCAL_LINUX_PART}
+  transfer_linux_to_client
 }
 
 function clone_admin_key {
-  dd if=/dev/sdb of=$1 bs=512 count=1
-  partprobe
-  umount /boot
-  partclone.extfs -b -s/dev/sdb1 -O${1}1
-  mount /boot
-  pvcreate ${1}2
-  vgcreate -s32M vg_adminflash_new ${1}2
-  lvcreate -l843 -n lv_root vg_adminflash_new
-  mount_admin_snap
-  umount /mnt
-  partclone.extfs -b -s/dev/vg_adminflash/player-root -O/dev/vg_adminflash_new/lv_root
-  umount_admin_snap
-  vgrename vg_adminflash vg_adminflash_orig
-  vgrename vg_adminflash_new vg_adminflash
-  vgchange -an vg_adminflash
-  sync
-  getkey -m "New key is ready, please unplug it and press any key"
-  vgrename vg_adminflash_orig vg_adminflash
+    dest=$1
+    if [ ! "${dest:0:5}"=="/dev/" ]; then
+	echo "clone_admin_key: Argument does not look like a device"
+    elif [ ! "$QCIMAGE_MODE" == "admin" ]; then
+	echo "clone_admin_key: Not available in ${QCIMAGE} mode"
+    else
+	dd if=${ADMIN_DISK} of=$1 bs=512 count=1
+	partprobe
+	partclone.btrfs -b -s${ADMIN_DISK} -O${dest}
+    fi
 }
 
 function sync_demos {
-  rsync -v -e "ssh -o StrictHostKeyChecking=no -l quakecon_web -i /etc/id_qctourney" $WINDOWS_DIR/Users/tourney-user/AppData/LocalLow/id\ Software/lan/home/baseq3/demos/* quakecon_web@static.quakecon.org:files/2011/demos
+  rsync -v -e "ssh -o StrictHostKeyChecking=no -l $DEMOS_USER -i $DEMOS_KEY" $WINDOWS_DIR/Users/tourney-user/AppData/LocalLow/id\ Software/lan/home/baseq3/demos/* $DEMOS_USER@$DEMOS_SERVER:$DEMOS_PATH
 }
 
-function copy_demo_key {
-  mount /dev/sdb1 /mnt
-  cp /mnt/id_qctourney /etc
-  chmod 600 /etc/id_qctourney
-  umount /mnt 
+function make_image_server_dir {
+    dest=$1
+    if [ ! -e $dest ]; then
+	mkdir $dest
+    fi   
+}
+
+function get_ntfs_cluster_size {
+    ntfsinfo -m $WINDOWS_PART | awk '/Cluster Size:/ { print $3 }'
+}
+
+function get_ntfs_num_clusters {
+    ntfsinfo -m $WINDOWS_PART | awk '/Size in Clusters:/ { print $5 }'
+}
+
+function compare_image_update_in_place {
+    new_image=$1
+    old_image=$2 # Will be modified
+    cluster_size=$(get_ntfs_cluster_size)
+    num_clusters=$(get_ntfs_num_clusters)
+    block_size=2000 # (In clusters e.g. 1000=4M) 
+    mod_counter=0
+    for (( i=0; i < num_clusters; i+=block_size )); do
+	if [ $(( i+block_size )) -gt $num_clusters ]; then
+	    i=$(( num_clusters - block_size ))
+	fi
+	offset=$((cluster_size*i))
+	old_md5=$(dd if=$old_image bs=$cluster_size count=$block_size skip=$offset 2>/dev/null|md5sum)
+	new_md5=$(dd if=$new_image bs=$cluster_size count=$block_size skip=$offset 2>/dev/null|md5sum)
+	if [ $(( i % ((num_clusters/100)/block_size) )) -eq 0 ]; then
+	    percent=$(( i*100/num_clusters ))
+	    mod_bytes=$(( mod_counter*cluster_size*block_size ))
+	    echo -ne "At cluster $i/$num_clusters ($percent%);"\
+		 "modified $mod_counter blocks ($mod_bytes bytes)\r"
+	fi
+	if [ "$old_md5" == "$new_md5" ]; then
+	    continue
+	else
+	    echo "Modified block at cluster $i (offset: $offset); updating image"
+	    dd if=$new_image of=$old_image skip=$offset bs=$cluster_size count=1 conv=notrunc 2>/dev/null
+	    mod_counter=$((mod_counter+1))
+	fi
+    done
+    echo "compare_image_update_in_place: Done"
+}
+
+function gen_btrfs_send {
+    parent_snap=$1
+    target_snap=$2
+    outfile=${SNAPSHOT_DIR}/${parent_snap}-${target_snap}.btrsend.lzo
+    btrfs send -p $parent_snap $target_snap | lzo -o $outfile
+}
+
+function get_newest_milestone {
+    echo $(get_milestones | tail -n1)
+}
+
+function get_oldest_milestone {
+    echo $(get_milestones | head -n1)
+}
+
+function get_milestones {
+    btrfs subvolume list $SNAPSHOT_DIR | awk '/milestone/ { print $9 }'
+}
+
+function target_snap_from_bsend_file {
+    filename=$1
+    echo $(echo $filename | sed -n 's/^[mileston0-9_]*-\(milestone\_[0-9]*\)\.bsend\.lzo$/\1/p')
+}
+
+function gen_milestone_server_dir {
+    if [ ! -e $MILESTONE_SRV_DIR ]; then
+	mkdir -p $MILESTONE_SRV_DIR
+    fi
+    (cd $MILESTONE_SRV_DIR
+     target=$(get_newest_milestone)
+     echo "Newest milestone is: $target"
+     if [ -e LATEST ]; then
+	 if [ ! $(cat LATEST) == "$target" ]; then
+	     echo "New milestone detected, deleting deprecated files"
+	     rm $MILESTONE_SRV_DIR/*
+	 fi
+     fi
+     echo $target > LATEST
+     parents="$(get_milestones) /" 
+     for parent in $parents; do
+	 # Wankery to correct for not having a parent name for an
+	 # initial bsend file
+	 if [ "${parent:0:1}" == "/" ]; then
+	     parent=${parent:1}
+	     outfile=${target}.bsend.lzo
+	 else
+	     outfile=${parent}-${target}.bsend.lzo
+	 fi
+	 # Don't try to generate a diff between a snapshot and itself
+	 if [ "$parent" == "$target" ]; then
+	     continue
+	 fi
+	 if [ ! -e $outfile ]; then
+	     echo "Generating $outfile"
+	     if [ -z "$parent" ]; then
+		 # No parent, send all the datas
+		 btrfs send $SNAPSHOT_DIR/$target | lzop -o $outfile
+	     else
+		 # Diff to upgrade one snap to the next
+		 btrfs send -p $SNAPSHOT_DIR/$parent $SNAPSHOT_DIR/$target | lzop -o $outfile
+	     fi
+	     md5sum -- "$outfile" >> MD5SUMS
+	 else
+	     echo "Found existing $outfile, skipping"
+	 fi
+    done
+    )
 }
--- shell/settings.sh
+++ shell/settings.sh
@@ -1,53 +1,91 @@
 # This file holds the tunable variables, basically everything I
 # thought might conceivably change 
 
-INTERNAL_DISK=/dev/sda
-PLAYER_DISK=/dev/sdb1
+DISKS=$(lsblk -o TYPE,NAME|awk '/disk/ {print $2}')
+INTERNAL_DISK=
+WINDOWS_PART=
+WINDOWS_SHIT=( pagefile.sys hiberfile.sys )
+LOCAL_LINUX_PART=
+LOCAL_LINUX_DIR=/client_linux
+ADMIN_LINUX_PART=
+ROOT_PART=$(mount|awk '/on \/ / {print $1}')
+#PLAYER_DISK=/dev/sdb1
 RAW_IMAGE_DIR=/images
-PLAYER_DIR=/joueur
+#PLAYER_DIR=/joueur
 WINDOWS_DIR=/windows
 REPO_DIR=/repo
+SNAPSHOT_DIR=/snapshots
 QCIMAGE_MODE=
-HANDLE=
-GUID=
-DIFF=
 
-function init_player_settings {
-    if [ ! -e $PLAYER_DIR/.qcimage ]; then
-	HANDLE=`cat $WINDOWS_DIR/.qcimage/handle`
-	GUID=`cat $WINDOWS_DIR/.qcimage/guid`
-	DIFF=${PLAYER_DIR}/.qcimage/diff
+# Where we host bsend files, must be on subvolume to avoid inclusion
+# in future milstone snaps
+MILESTONE_SRV_DIR=$SNAPSHOT_DIR/milestone_srv
+
+# Rsync info for demo backup
+DEMOS_USER=
+DEMOS_KEY=
+DEMOS_SERVER=
+DEMOS_PATH=
+
+#HANDLE=
+#GUID=
+#DIFF=
+
+#function init_player_settings {
+#    if [ ! -e $PLAYER_DIR/.qcimage ]; then
+#	HANDLE=`cat $WINDOWS_DIR/.qcimage/handle`
+#	GUID=`cat $WINDOWS_DIR/.qcimage/guid`
+#	DIFF=${PLAYER_DIR}/.qcimage/diff
+#    else
+#	# The absence of the directory indicates image creation
+#	HANDLE=`cat ${PLAYER_DIR}/.qcimage/handle`
+#	GUID=`cat ${PLAYER_DIR}/.qcimage/guid`
+#	DIFF=${PLAYER_DIR}/.qcimage/diff
+#    fi
+#}
+
+function get_partition_fstype {
+    part=$1
+    if [ -n $part ]; then
+	if [ "${part:0:5}" == "/dev/" ]; then
+	    part=${part:5}
+	fi
+	part=$(lsblk -o NAME,FSTYPE|awk -v part="$part" 'index($0, part) {print $2}')
+    fi
+    echo $part
+}
+
+function find_internal_disk {
+    # Find the first enumerated disk whose first partition is ntfs and
+    # contains a "Windows" folder.
+    for disk in $DISKS; do
+	disk_dev=/dev/$disk
+	first_part_fstype=$(get_partition_fstype ${disk_dev}1)
+	echo "Checking $disk @ $disk_dev, type $first_part_fstype"
+	if [ "${first_part_fstype}" == "ntfs" ]; then
+	    tmpdir=$(mktemp -d)
+	    mount ${disk_dev}1 ${tmpdir}
+	    if [ -e ${tmpdir}/Windows ]; then
+		INTERNAL_DISK=${disk_dev}
+		WINDOWS_PART=${INTERNAL_DISK}1
+		LOCAL_LINUX_PART=${INTERNAL_DISK}2
+	    fi
+	    umount ${tmpdir} && rm -r ${tmpdir}
+	fi
+    done
+}
+
+function detect_mode {
+    if [ "$ROOT_PART" == "$LOCAL_LINUX_PART" ]; then
+	# We're booted from the linux on the internal disk
+	QCIMAGE_MODE="local"
     else
-	# The absence of the directory indicates image creation
-	HANDLE=`cat ${PLAYER_DIR}/.qcimage/handle`
-	GUID=`cat ${PLAYER_DIR}/.qcimage/guid`
-	DIFF=${PLAYER_DIR}/.qcimage/diff
+	QCIMAGE_MODE="admin"
+	ADMIN_LINUX_PART=$ROOT_PART
     fi
 }
 
-function disk_type {
-	# Expects a mounted disk device as arg 1 returns fstype
-	mount -l |grep $1|cut -d" " -f5 -
-}
-
-function detect_settings {
-	# See if what should be the player disk is actually /boot of the admin flash
-	if [ "`disk_type ${PLAYER_DISK}1`" == "ext4" ]; then
-		QCIMAGE_MODE="admin"
-	else
-		QCIMAGE_MODE="player"
-	fi
-	if [ "$QCIMAGE_MODE" == "admin" ]; then
-		PLAYER_DISK=
-		PLAYER_DIR=
-	else
-		if [ -z "`mount -l -t vfat`" ]; then
-			mount $PLAYER_DIR
-		fi
-		init_player_settings
-	fi
-}
-
-detect_settings
-export QCIMAGE_MODE INTERNAL_DISK PLAYER_DISK RAW_IMAGE_DIR PLAYER_DIR REPO_DIR WINDOWS_DIR
-export HANDLE GUID DIFF
+detect_mode
+find_internal_disk
+export QCIMAGE_MODE INTERNAL_DISK RAW_IMAGE_DIR REPO_DIR WINDOWS_DIR SNAPSHOT_DIR
+export DISKS WINDOWS_PART LOCAL_LINUX_PART ADMIN_LINUX_PART ROOT_PART
